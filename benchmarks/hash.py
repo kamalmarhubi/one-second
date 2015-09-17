973c9091c53121e3a61a69e003b8791a8c03130c
Julia Evans <julia@jvns.ca>
2015-09-17 14:19:32 -0700

    bytes hashed in a second

---
+++ benchmarks/hash.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+
+import hashlib
+
+CHUNK_SIZE = 10000
+s = 'a' * CHUNK_SIZE
+
+# How many bytes can we hash in a second?
+def f(NUMBER):
+    bytes_hashed = 0
+    h = hashlib.md5()
+    while bytes_hashed < NUMBER:
+        h.update(s)
+        bytes_hashed += CHUNK_SIZE
+    h.digest()
+import sys
+f(int(sys.argv[1]))
