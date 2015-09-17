a23033acae71c0575515793a8accfbc9e0e828fc
Julia Evans <julia@jvns.ca>
2015-09-17 15:41:22 -0700

    parse http request benchmark

---
+++ benchmarks/parse_http_request.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+
+from BaseHTTPServer import BaseHTTPRequestHandler
+from StringIO import StringIO
+
+class HTTPRequest(BaseHTTPRequestHandler):
+    def __init__(self, request_text):
+        self.rfile = StringIO(request_text)
+        self.raw_requestline = self.rfile.readline()
+        self.error_code = self.error_message = None
+        self.parse_request()
+
+    def send_error(self, code, message):
+        self.error_code = code
+        self.error_message = message
+
+request_text = """GET / HTTP/1.1
+Host: localhost:8001
+Connection: keep-alive
+Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8
+Upgrade-Insecure-Requests: 1
+User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/45.0.2454.85 Safari/537.36
+Accept-Encoding: gzip, deflate, sdch
+Accept-Language: en-GB,en-US;q=0.8,en;q=0.6
+"""
+
+def f(NUMBER):
+    for _ in range(NUMBER):
+        HTTPRequest(request_text)
+
+import sys
+f(int(sys.argv[1]))
