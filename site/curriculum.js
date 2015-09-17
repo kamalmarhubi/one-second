47400f4338c5cddda9e5a6f833a554d76cda11c5
Kamal Marhubi <kamal@marhubi.com>
2015-09-17 18:45:30 +0200

    Move curriculum to its own file

---
+++ site/curriculum.js
@@ -0,0 +1,16 @@
+const curriculum = [
+    {
+        'text': 'Welcome to the first program! This one is just to get you on your feet: how many loops can you go through in a second? (it might be more than you think!)',
+        'programs': ["sum.c", "loop.py"],
+    },
+    {
+        'text': ["Next up, we have downloading a webpage vs running a Python script!"],
+        'programs': ["download_webpage.py", "run_python.sh"],
+    },
+    {
+        'text': ["How many bytes can you write to disk in a second? We all know writing to memory is faster, but how *much* faster? This code was run on a computer with an SSD."],
+        'programs': ["write_to_disk.py", "write_to_memory.py"]
+    }
+]
+
+export default curriculum
