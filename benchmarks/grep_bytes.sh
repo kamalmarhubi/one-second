f46843d8e1eeae49cd0e497b9c054fa1de545961
Julia Evans <julia@jvns.ca>
2015-09-18 12:18:43 -0700

    grep bytes

---
+++ benchmarks/grep_bytes.sh
@@ -0,0 +1,9 @@
+#!/bin/bash 
+
+# Number to guess: How many bytes can `grep`
+# search, unsuccessfully, in a second?
+# Note: the bytes are in memory
+
+NUMBER=$1
+
+cat /dev/zero | head -c $NUMBER | grep blah
