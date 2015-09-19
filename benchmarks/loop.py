4a2d20ec368e2a9368f889aaa80ef62f459219c3
Julia Evans <julia@jvns.ca>
2015-09-19 03:03:28 -0700

    fix up comments

--- benchmarks/loop.py
+++ benchmarks/loop.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
-# Number to guess: How many iterations of an empty loop can we go through in a second?
+# Number to guess: How many iterations of an
+# empty loop can we go through in a second?
 
 def f(NUMBER):
     for _ in xrange(NUMBER):
--- benchmarks/sum.c
+++ benchmarks/sum.c
@@ -1,6 +1,7 @@
 #include <stdlib.h>
 
-// Number to guess: How many iterations of this loop can we go through in a second?
+// Number to guess: How many iterations of
+// this loop can we go through in a second?
 
 int main(int argc, char **argv) {
     int NUMBER, i, s;
