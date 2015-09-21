2a31de6bf00ebbaff5d7f0ff2a0ddbf6a0553400
Julia Evans <julia@jvns.ca>
2015-09-21 01:00:59 -0700

    more curriculum

--- site/curriculum.js
+++ site/curriculum.js
@@ -4,6 +4,11 @@ const curriculum = [
          your feet: how many loops can you go through in a second? (it might
          be more than you think!)</p>`,
         'programs': ["sum.c", "loop.py"],
+        'conclusion': `
+            When I see code like this, I think about how much time a millisecond is 
+            -- it's imperceptible to me, but apparently you can run 68,000
+            iterations of an empty loop in a millisecond, even in Python!
+        `
     },
     {
 
@@ -19,11 +24,29 @@ const curriculum = [
          `,
 
         'programs': ["dict.py", "parse_http_request.py"],
+        'conclusion': `
+            We can parse 25,000 small HTTP requests per second! One thing I want to
+            point out here is that the request parsing code is written in pure Python,
+            not C -- so we can actually do a quite nontrivial amount of work in
+            40 microseconds.
+        `
     },
     {
         'text': `<p>Next up, we have downloading a webpage vs running a Python script! 
             Hint: these are both less than 100 million :)</p>`,
         'programs': ["download_webpage.py", "run_python.sh"],
+        'conclusion': `
+            Starting a program is actually already expensive by itself, not just starting Python.
+            If we just run /bin/true, we can do 500 of them in a second, so it looks like
+            running any program has about 1ms of overhead. The cost of downloading a webpage of course
+            depends a lot the size, connection speed, and distance from the servers, 
+            and we're not going to get further into network performance 
+            (network performance is SO INTERESTING). Friends who do high performance networking
+            say it's possible to get network roundtrips of 250ns (!!!), 
+            but that's with much closer computers, and fancier hardware.
+            For us and Google, it's a million times longer </3>.
+            Light can only travel a foot in a nanosecond, and Google is much more than 250 feet away.
+        `
     },
     {
         'text': `
@@ -45,7 +68,7 @@ const curriculum = [
         `,
         'conclusion': `
             Great! Now we know that grep can search at 2GB/s, so at least in this case,
-            we're limited more by the disk speed than grep's speed`,
+            we're limited more by the disk speed than grep's speed.`,
 
         'programs': ["grep_bytes.sh", "find-filenames.sh"],
     },
@@ -60,6 +83,12 @@ const curriculum = [
             </p>
         `,
         'programs': ["json_parse.py", "msgpack_parse.py"],
+        'conclusion': `
+            Basically every single person we talked to about serialization mentioned that capnproto
+            does instant serialization. We just want you to know that deserializing
+            64K of data can take a millisecond (a LONG TIME, as we now know), and
+            that your choice of format and library makes a big difference.
+        `
     },    {
         'text': `
             <p>
@@ -69,6 +98,11 @@ const curriculum = [
             </p>
         `,
         'programs': ["database_indexed.py", "database_unindexed.py"],
+        'conclusion': `
+            Not a big surprise: indexes are amazing. 20ish microseconds for an indexed query
+            means that if this were over a network to a faraway database server,
+            the time to query would be dominated by the network roundtrip time to the server.
+        `
     },
     {
         'text': `
@@ -79,18 +113,29 @@ const curriculum = [
             </p>
         `,
         'programs': ["hash.py", "bcrypt_hash.py"]
+        'conclusion': `MD5 is real fast: I won't complain about 400MB/s.`
     },
     {
         'text': `
             <p>
-            Next up, let's talk about memory access. Are memory accesses
-            expensive? (yes they are!). Let's see how accessing memory in-order vs
-            out-of-order affects performance. You might want to refer to 
+            Next up, let's talk about memory access. CPUs these days have L1 and L2 caches,
+            which are much faster to access than main memory. This means that accessing memory
+            sequentially (where the CPU can load a bunch of data into a cache) will normally give you faster code than
+            accessing memory out of order.
+            </p>
+            <p>
+            Let's see how that shakes out in practice! You might want to refer to 
             <a href="https://gist.github.com/jboner/2841832">Latency Numbers Every Programmer Should Know</a>
-            to guess at this one. (L1 cache reference vs main memory reference)
+            to guess at this one.
             </p>
         `,
         'programs': ["fill_array.c", "fill_array_out_of_order.c"]
+        'conclusion': `
+            We don't write a lot of C, so this doesn't affect us very often.
+            But if you care about how many nanoseconds your operations take
+            (which you do if you're trying to do a billion things a second, which computers can),
+            you care about this kind of thing a lot.
+        `
     },
     {
         'text': `
