010577cf72d4115b7a3b519a83db719916409a8e
David <auscompgeek@users.noreply.github.com>
2015-10-26 00:01:17 +1100

    curriculum: fix link to msgpack.org
    * msgpack.org has a valid, trusted SSL certificate; so does GH Pages.
      If we're loaded over HTTPS, the user probably wants outbound links
      to also load over HTTPS.
    
    * Nobody should be linking to /index.html.

--- site/curriculum.js
+++ site/curriculum.js
@@ -95,7 +95,7 @@ const curriculum = [
             it can really hurt, especially if you end up serializing/deserializing
             the same data repeatedly.
             Here are a couple of benchmarks: of parsing 64K of JSON, and the
-            same data encoded in <a href="http://msgpack.org/index.html">msgpack</a> format.
+            same data encoded in <a href="//msgpack.org">msgpack</a> format.
             </p>
         `,
         'programs': {"json_parse.py": "iterations", "msgpack_parse.py": "iterations"},
