ba310b1d07a270ec402a0b5b4de9a8c76aadfae3
Julia Evans <julia@jvns.ca>
2015-10-24 22:46:18 -0400

    don't nuke the node modules every time when deploying

--- deploy.sh
+++ deploy.sh
@@ -9,6 +9,7 @@ npm install
 node_modules/.bin/jspm unbundle
 node_modules/.bin/jspm bundle main site/bundle.js --inject --minify
 TMPDIR=`mktemp -d`
+mv node_modules $TMPDIR
 mv site/ $TMPDIR
 git checkout gh-pages
 rm -rf $PWD/*
@@ -17,3 +18,4 @@ git add .
 git commit -a -m"Update site"
 git push deploy gh-pages:master
 git checkout master
+mv $TMPDIR/node_modules .
