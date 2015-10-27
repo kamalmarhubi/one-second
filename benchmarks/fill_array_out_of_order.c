99cab0d66812d9e1cf5a3fadc419c68a19f4751f
Ian C <ilc.ianclark@gmail.com>
2015-10-27 09:07:31 -0500

    Removing unneccessary printf() and import
    Removed b/c the volatile keyword will ensure the loop isn't -o2'ed out.

--- benchmarks/fill_array_out_of_order.c
+++ benchmarks/fill_array_out_of_order.c
@@ -1,25 +1,21 @@
 #include <stdlib.h>
-#include <stdio.h>
 
 // Number to guess: How big of an array (in bytes)
 // can we allocate and fill with 5s in a second?
 // The catch: We do it out of order instead of in order.
 int main(int argc, char **argv) {
-    int NUMBER, i;
-    NUMBER = atoi(argv[1]);
+    volatile int i = 0;
+    const unsigned int NUMBER = atoi(argv[1]);
 
     char* array = malloc(NUMBER);
     int j = 1;
-    for (i = 0; i < NUMBER; ++i) {
+    for (i; i < NUMBER; ++i) {
         j = j * 2;
         if (j > NUMBER) {
             j = j - NUMBER;
         }
         array[j] = j;
     }
-
-    printf("%d", array[NUMBER / 7]);
-    // so that -O2 doesn't optimize out the loop
 
     return 0;
 }
